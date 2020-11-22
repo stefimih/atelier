d9af5ecb4455a5d1b8825c7f57703ceb9609e137
stefimih <stefimih@users.noreply.github.com>
2020-11-22 23:33:00 +0200

    inital commit

---
+++ tema1.py
@@ -0,0 +1,36 @@
+# cerinta1
+print("Cerinta1\n")
+my_list = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
+a = my_list[:]
+print(my_list)
+print("\n")
+
+# cerinta2
+print("Cerinta2\n")
+a.sort()
+b = a[:]
+print(a)
+print("\n")
+
+# cerinta3
+print("Cerinta3\n")
+b.reverse()
+print(b)
+print("\n")
+
+# cerinta4
+print("Cerinta4\n")
+my_list_even = a[1:11:2]
+print(my_list_even)
+print("\n")
+
+# cerinta5
+print("Cerinta5\n")
+my_list_odd = a[0:10:2]
+print(my_list_odd)
+print("\n")
+
+# cerinta
+print("Cerinta6\n")
+my_list_3multiple = a[2:9:3]
+print(my_list_3multiple)
